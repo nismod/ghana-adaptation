b33ae442882f2fb29ec6d045896e80f7e8dc130c
Tom Russell <tomalrussell@gmail.com>
2020-11-03 12:38:25 +0000

    Update intersection for faster processing
    Use spatial index, report progress via tqdm and CSV intermediate output

--- src/exposure/intersection.py
+++ src/exposure/intersection.py
@@ -2,11 +2,13 @@
 
 Calculate the intersection of hazards and infrastructure networks.
 """
+import csv
 import glob
 import json
 import os
 
 import geopandas
+from tqdm import tqdm
 
 
 def main():
@@ -14,36 +16,74 @@ def main():
     """
     config = load_config()
     base_path = config['base_path']
+    epsg_code = 32630 # 32630 is UTM Zone 30N, suitable for Ghana
 
     # Setting up the paths
     hazard_paths = glob.glob(
-        os.path.join(base_path, 'data', 'hazard', 'flood_outline', '*.gpkg'))
+        os.path.join(base_path, 'data', 'hazard_fl_pres', '*.shp'))
 
     network_path = os.path.join(
-        base_path, 'incoming', 'transport', 'Roads',
-        'ID 37 GAMA Roads - Ghana Open Data Initiative', 'GAMA Roads',
-        'GAMA_Roads.shp')
+        base_path, 'data', 'infrastructure_pres', 'GHA_highway.shp')
 
-    # Reading infrastructure network
-    network_df = geopandas.read_file(network_path)
+    # Reading infrastructure network, convert to projected CRS
+    network_df = geopandas.read_file(network_path).to_crs(epsg=epsg_code)
 
     for hazard_path in hazard_paths:
-        hazard_id = os.path.basename(hazard_path).replace(".gpkg", "")
+        hazard_id = os.path.basename(hazard_path).replace(".shp", "")
+
         print("Processing", hazard_id)
 
         # Reading hazard outlines
         hazard_df = geopandas.read_file(hazard_path)
 
-        # convert to 32630
-        hazard_df = hazard_df.to_crs(epsg=32630)
+        # Convert to projected CRS
+        hazard_df = hazard_df.to_crs(epsg=epsg_code)
 
         # Do intersection
-        intersection_df = geopandas.overlay(network_df, hazard_df, how='intersection')
+        intersections = []
+        csv_fname = os.path.join(
+            base_path, 'results', 'exposure', f"roads__{hazard_id}.csv")
+
+        with open(csv_fname, 'w') as fh:
+            w = csv.DictWriter(fh, fieldnames=('road_id', 'hazard_id', 'name', 'length'))
+            w.writeheader()
+
+            for hazard_id, hazard in tqdm(enumerate(hazard_df.itertuples()), total=len(hazard_df)):
+                # Try fixing invalid geometry
+                if not hazard.geometry.is_valid:
+                    hazard_geom = hazard.geometry.buffer(0)
+                else:
+                    hazard_geom = hazard.geometry
+
+                # Use spatial index to find candidate road segments
+                potential_roads = network_df.iloc[
+                    list(network_df.sindex.intersection(hazard_geom.bounds))]
+
+                if len(potential_roads):
+                    for road in tqdm(potential_roads.itertuples(), total=len(potential_roads)):
+                        if road.geometry.intersects(hazard_geom):
+                            intersection_geom = road.geometry.intersection(hazard_geom)
+                            w.writerow({
+                                'road_id': road.ID,
+                                'hazard_id': hazard_id,
+                                'name': road.NAME,
+                                'length': intersection_geom.length
+                            })
+                            intersections.append({
+                                'road_id': road.ID,
+                                'hazard_id': hazard_id,
+                                'name': road.NAME,
+                                'length': intersection_geom.length,
+                                'geometry': intersection_geom
+                            })
+
+                    fh.flush()
 
         # Write intersection data
-        intersection_df.to_file(
-            os.path.join(base_path, 'results', 'exposure', f"roads__{hazard_id}.gpkg"),
-            driver="GPKG")
+        fname = os.path.join(
+            base_path, 'results', 'exposure', f"roads__{hazard_id}.gpkg")
+        intersections_df = geopandas.GeoDataFrame(intersections).set_crs(epsg=epsg_code)
+        intersections_df.to_file(fname, driver="GPKG")
 
 
 def load_config():
