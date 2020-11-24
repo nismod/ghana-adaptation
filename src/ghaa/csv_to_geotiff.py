16ccc0b06af3145148f21533b7219789d08820a8
Tom Russell <tomalrussell@gmail.com>
2020-11-24 17:03:10 +0000

    Use index for itertuples

--- src/ghaa/csv_to_geotiff.py
+++ src/ghaa/csv_to_geotiff.py
@@ -22,9 +22,15 @@ def convert_csv_to_tiff(template_path, csv_path, tif_path, value_col='val',
     df = pandas.read_csv(
         csv_path, usecols=[value_col, x_col, y_col])
 
+    # Enumerate columns to use index in itertuples
+    idx = {name: i for i, name in enumerate(df.columns, start=1)}
+    x_idx = idx[x_col]
+    y_idx = idx[y_col]
+    value_idx = idx[value_col]
+
     for obs in df.itertuples():
-        row, col = ds.index(obs[x_col], obs[y_col])
-        arr[row, col] = obs[value_col]
+        row, col = ds.index(obs[x_idx], obs[y_idx])
+        arr[row, col] = obs[value_idx]
 
 
     with rasterio.open(
