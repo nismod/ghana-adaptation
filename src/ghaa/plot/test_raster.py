1547544d7328e4c65933e23738a082bedb37b07d
Tom Russell <tomalrussell@gmail.com>
2020-11-25 18:23:56 +0000

    Accept command line args in plot raster script

--- src/ghaa/plot/test_raster.py
+++ src/ghaa/plot/test_raster.py
@@ -1,30 +1,33 @@
 import os
+import sys
+
 import matplotlib.pyplot as plt
 
 from ghaa.config import load_config
-from ghaa.plot.map import get_axes_for_raster, plot_raster
+from ghaa.plot.map import get_axes, plot_basemap, plot_raster
 
 
 if __name__ == '__main__':
     base_path = load_config()["base_path"]
+    try:
+        tif_path = sys.argv[1]
+    except:
+        # GeoTIFF to plot
+        tif_path = os.path.join(
+            base_path, 'results', 'proximity_results',
+            'population_gha_2019-07-01_proximity_downsampled.tif')
 
-    # GeoTIFF to plot
-    tif_path = os.path.join(
-        base_path, 'results', 'proximity_results',
-        'population_gha_2019-07-01_proximity_downsampled.tif')
-    ax = get_axes_for_raster()
-    print("got axes")
+    try:
+        png_path = sys.argv[2]
+    except:
+        # PNG output - default to same name as TIF, but .png
+        png_path = os.path.join(
+            os.path.dirname(tif_path),
+            os.path.basename(tif_path).replace(".tif", ".png"))
+
+    print("Plotting", tif_path)
+    print("to image", png_path)
+    ax = get_axes()
     plot_raster(ax, tif_path, base_path)
-    print("plotted")
-    plt.savefig('test_access.png')
-    print("saved")
-    exit()
-    # Accra
-    left = -0.5
-    bottom = 5.4
-    right = 0.1
-    top = 5.8
-    extent = (left, bottom, right, top)
-    ax = get_axes_for_raster()
-    plot_raster(ax, tif_path, base_path, extent=extent)
-    plt.savefig('test_access_accra.png')
+    plot_basemap(ax, os.path.join(base_path, 'data'))
+    plt.savefig(png_path)
